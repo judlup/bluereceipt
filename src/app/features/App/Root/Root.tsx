aaa390751ad4d4bde89a480ec89ed4ca43362144
Judlup Luns <judlup@gmail.com>
2020-06-07 09:20:27 -0500

    07-06-2020 | Intital commit | Judlup

---
+++ src/app/features/App/Root/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Input from '../../CoreUI/FormInput/FormInput'
+
+import './Root.scss';
+
+const Root: React.FC = () => (
+  <div>
+  Hello World...  
+  </div>
+);
+
+export default Root;
